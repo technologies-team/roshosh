bd8cb201e0954c5a52ee756c984a5bb829c61485
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-04-24 16:09:08 +0400

    add phone & verified to location

--- database/seeders/AttachmentsTableSeeder.php
+++ database/seeders/AttachmentsTableSeeder.php
@@ -15,34 +15,34 @@ class AttachmentsTableSeeder extends Seeder
     {
         DB::table('attachments')->insert([
             [
-                'name' => 'V5rJO5f3Wesx1YlXsFImgkAkj6VcM1X90QJzwvdA.jpg',
+                'name' => 'V5rJO5f3Wesx1YlXsFImgkAkj6VcM1X90QJzwvdA.png',
                 'mime_type' => 'application/pdf',
                 'path' => 'attachment/V5rJO5f3Wesx1YlXsFImgkAkj6VcM1X90QJzwvdA.jpg',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'name' => 'V5rJO5f3Wesx1YlXsFImgkAkj6VcM1X90QJzwvdA.jpg',
+                'name' => 'V5rJO5f3Wesx1YlXsFImgkAkj6VcM1X90QJzwvdA.png',
                 'mime_type' => 'image/jpeg',
                 'path' => '/attachment/V5rJO5f3Wesx1YlXsFImgkAkj6VcM1X90QJzwvdA.jpg',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],   [
-                'name' => 'EsjgKDQW6RrNSllXyXVvGVkkZmtVUqkuHq6HjDb9.jpg',
+                'name' => 'EsjgKDQW6RrNSllXyXVvGVkkZmtVUqkuHq6HjDb9.png',
                 'mime_type' => 'image/jpeg',
-                'path' => '/attachment/EsjgKDQW6RrNSllXyXVvGVkkZmtVUqkuHq6HjDb9.jpg',
+                'path' => '/attachment/EsjgKDQW6RrNSllXyXVvGVkkZmtVUqkuHq6HjDb9.png',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],   [
-                'name' => 'HwrSFp81LtNzk4fz6PUiTIDeehQrgkfoQOjyU6r6.jpg',
+                'name' => 'HwrSFp81LtNzk4fz6PUiTIDeehQrgkfoQOjyU6r6.png',
                 'mime_type' => 'image/jpeg',
-                'path' => '/attachment/HwrSFp81LtNzk4fz6PUiTIDeehQrgkfoQOjyU6r6.jpg',
+                'path' => '/attachment/HwrSFp81LtNzk4fz6PUiTIDeehQrgkfoQOjyU6r6.png',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],   [
-                'name' => 'tnwbu3EnH0BABqvsDtjn8xV94PwSC0TCfKqdNZua.jpg',
+                'name' => 'tnwbu3EnH0BABqvsDtjn8xV94PwSC0TCfKqdNZua.png',
                 'mime_type' => 'image/jpeg',
-                'path' => '/attachment/tnwbu3EnH0BABqvsDtjn8xV94PwSC0TCfKqdNZua.jpg',
+                'path' => '/attachment/tnwbu3EnH0BABqvsDtjn8xV94PwSC0TCfKqdNZua.png',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],

93daaee7f28395507d7b4306a19b4c352807ebda
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-05-15 15:00:02 +0400

    fix text

--- database/seeders/ServicesTableSeeder.php
+++ database/seeders/ServicesTableSeeder.php
@@ -18,8 +18,8 @@ class ServicesTableSeeder extends Seeder
                 'title' => 'Standard Wash',
                 'title_ar' => 'غسيل عادي',
                 'description' => 'Several stages are involved:
-    1- Interior Cleaning With Deep Vacuum and Shine.
-    2- Exterior Wash with high Pressure with Shampoo Tyers Clean.',
+1- Interior Cleaning With Deep Vacuum and Shine.
+2- Exterior Wash with high Pressure with Shampoo Tyers Clean.',
                 'description_ar' => 'تكون على عدة مراحل
      1-تنظيف داخلي  للسيارة مع شفط الأتربة والتلميع
      2-غسيل خارجي للسيارة بالضغط العالي بالشامبو وتنظيف الإطارات
@@ -36,10 +36,10 @@ class ServicesTableSeeder extends Seeder
                 'title' => 'Premium Wash',
                 'title_ar' => 'غسيل ممتاز',
                 'description' => 'Several stages are involved:
-    1- High Pressure Washing With Shampoo.
-    2- Engine Cleaning v Interior Deep Vacuum v Tyer and Rim Cleaning& Shining .
-    3- Glass Cleaning v Dashboard Shine .
-    4- AC duct steam Cleaning
+1- High Pressure Washing With Shampoo.
+2- Engine Cleaning v Interior Deep Vacuum v Tyer and Rim Cleaning& Shining .
+3- Glass Cleaning v Dashboard Shine .
+4- AC duct steam Cleaning
     ',
                 'description_ar' => 'تكون على عدة مراحل
     1-غسيل السيارة بالضغط العالي بالشامبو
@@ -58,18 +58,18 @@ class ServicesTableSeeder extends Seeder
                 'title' => 'Platinum',
                 'title_ar' => 'غسيل بلاتينيوم',
                 'description' => 'Several stages are involved:
-    1- Wax Shining.
-    2- External Plastic Cover
-    3- Interior Stain Remove
-    4- Interior Polishing
-    5- AC Cleaning
-    6- Tiers Polishing
-    7- Engine Wash and Polish
-    8- Exterior Shampoo
-    9- Washing & Interior Cleaning
+1- Wax Shining.
+2- External Plastic Cover
+3- Interior Stain Remove
+4- Interior Polishing
+5- AC Cleaning
+6- Tiers Polishing
+7- Engine Wash and Polish
+8- Exterior Shampoo
+9- Washing & Interior Cleaning
     ',
                 'description_ar' => 'تكون على عدة مراحل
-    1-تلميع جميع زجاج السيارة
+                1-تلميع جميع زجاج السيارة
     2- تغطية السيارة بأغطية بلاستيكية خارجية
     3- إزالة البقع الداخلية الموجودة في السيارة
     4- تلميع السيارة داخلياً
@@ -89,16 +89,16 @@ class ServicesTableSeeder extends Seeder
                 'title' => 'Car Detailing',
                 'title_ar' => 'خدمات التنظيف الداخلي والخارجي للسيارات (تنفيض السيارات)',
                 'description' => 'Several stages are involved:
-    1- Two Layer body wash(steam water)
-    2- Steps body polishing with high quality products
-    3- Full interior detailing with high pressure steam
-    4- Seat polishing shampooing engine compartment cleaning
-    5- AC duct cleaning with hot steam
-    6- Leather protection coating
-    7- Trunk vacuum
-    8- Truck mat cleaning and stain removal
-    9- Exterior Shampoo
-    10-Washing & Interior
+1- Two Layer body wash(steam water)
+2- Steps body polishing with high quality products
+3- Full interior detailing with high pressure steam
+4- Seat polishing shampooing engine compartment cleaning
+5- AC duct cleaning with hot steam
+6- Leather protection coating
+7- Trunk vacuum
+8- Truck mat cleaning and stain removal
+9- Exterior Shampoo
+10-Washing & Interior
     ',
                 'description_ar' => 'تكون على عدة مراحل
     1-غسيل السيارة على مرحلتين باستخدام البخار

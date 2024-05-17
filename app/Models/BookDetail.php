6c195c513029e4fd96d4aa75b3d53fa606db5abe
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-05-17 16:36:36 +0400

    clean

--- app/Models/BookDetail.php
+++ app/Models/BookDetail.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * @method static whereDate(string $string, \Carbon\Carbon|\Illuminate\Support\Carbon $currentDate)
@@ -12,4 +14,8 @@ class BookDetail extends Model
 {
 protected $fillable=['service_name','location','vehicle','coupon','service_time','finished_at'];
     use HasFactory;
+public function book(): BelongsTo
+{
+  return  $this->belongsTo(Book::class);
+}
 }

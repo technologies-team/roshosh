1ac4edb4054793e8cf8480bd6a5fe16994d49588
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-05-29 15:19:10 +0400

    add offer price to service

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Laravel 8 Mobile Number (OTP) Authentication using Firebase - websolutionstuff.com</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>

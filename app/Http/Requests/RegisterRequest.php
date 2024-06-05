d6dcccaf74010fe7f2bb3bfb58d754aad42f14ff
muhammad-Qanah <103624694+muhammad-Qanah@users.noreply.github.com>
2024-06-05 11:28:01 +0400

    add to user details

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -11,7 +11,7 @@ class RegisterRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
